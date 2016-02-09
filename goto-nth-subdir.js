3b4fef95c737c30902ed48287c0dc839ea2d0374
elpinal <elpinal@users.noreply.github.com>
2016-02-09 19:00:41 +0900

    Press 1< to go to root/subdir1
    Press < to go to root/

--- goto-nth-subdir.js
+++ goto-nth-subdir.js
@@ -17,8 +17,8 @@
 liberator.plugins.exGoToNthSubdir = (function(){
   commands.addUserCommand(['goto'], 'Go to nth subdir',
       function (args) {
-        let n = 1;
-        if (args > 1) {
+        let n = 0;
+        if (args > 0) {
           n = count;
         }
         liberator.plugins.exGoToNthSubdir.goto(n);
@@ -26,14 +26,14 @@ liberator.plugins.exGoToNthSubdir = (function(){
 
   mappings.add([modes.NORMAL], ["<"],
       "Go to nth subdir",
-      function (count) { liberator.plugins.exGoToNthSubdir.goto(Math.max(count, 1)); },
+      function (count) { liberator.plugins.exGoToNthSubdir.goto(Math.max(count, 0)); },
       { count: true });
 
   return {
     goto: function goto(count)
     {
         let uri = content.document.location;
-        let subdir = uri.pathname.split("/").slice(1, count).join("/");
+        let subdir = uri.pathname.split("/").slice(1, count + 1).join("/");
         liberator.assert(!/(about|mailto):/.test(uri.protocol)); // exclude these special protocols for now
         liberator.open(uri.protocol + "//" + (uri.host || "") + "/" + subdir);
     }
