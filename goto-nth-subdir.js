33f4ae98fafc94e630d14b0ce299476dca11507d
elpinal <elpinal@users.noreply.github.com>
2016-04-08 16:04:55 +0900

    Clean up

--- goto-nth-subdir.js
+++ goto-nth-subdir.js
@@ -1,41 +1,26 @@
-/*
- * @name goto-nth-subdir.js
- * @description	"Go to the root/[n-depth-subdir] of the website"
- * @author lukana
- * @version 0.0.1
- *
- * LICENSE
- *   Public Domain http://creativecommons.org/licenses/publicdomain/
- *
- * USAGE
- *   :goto <count>
- *
- *   <count><
- * 
- */
-
-liberator.plugins.exGoToNthSubdir = (function(){
-  commands.addUserCommand(['goto'], 'Go to nth subdir',
-      function (args) {
-        let n = 0;
-        if (args > 0) {
-          n = count;
-        }
-        liberator.plugins.exGoToNthSubdir.goto(n);
-      });
+liberator.plugins.exGoToNthSubdir = (function() {
+  commands.addUserCommand(['goto'], 'Go to nth subdir', function(args) {
+    let n = 0;
+    if (args > 0) {
+      n = count;
+    }
+    liberator.plugins.exGoToNthSubdir.goto(n);
+  });
 
   mappings.add([modes.NORMAL], ["<"],
-      "Go to nth subdir",
-      function (count) { liberator.plugins.exGoToNthSubdir.goto(Math.max(count, 0)); },
-      { count: true });
+    "Go to nth subdir", function(count) {
+      liberator.plugins.exGoToNthSubdir.goto(Math.max(count, 0));
+    },
+    {
+      count: true
+    });
 
   return {
-    goto: function goto(count)
-    {
-        let uri = content.document.location;
-        let subdir = uri.pathname.split("/").slice(1, count + 1).join("/");
-        liberator.assert(!/(about|mailto):/.test(uri.protocol)); // exclude these special protocols for now
-        liberator.open(uri.protocol + "//" + (uri.host || "") + "/" + subdir);
+    goto: function goto(count) {
+      let uri = content.document.location;
+      let subdir = uri.pathname.split("/").slice(1, count + 1).join("/");
+      liberator.assert(!/(about|mailto):/.test(uri.protocol)); // exclude these special protocols for now
+      liberator.open(uri.protocol + "//" + (uri.host || "") + "/" + subdir);
     }
   };
 })();
