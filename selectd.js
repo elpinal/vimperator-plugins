34829ee8c02b4e4f8374b2fde16a4721d7602246
elpinal <elpinal@users.noreply.github.com>
2016-04-09 22:27:24 +0900

    Update

--- selectd.js
+++ selectd.js
@@ -1,14 +1,14 @@
-liberator.plugins.exSelectd = (function() {
+(function() {
   commands.addUserCommand(['selectd', 'sd'], 'Close selected tabs', function(args) {
     if (args != "") {
       let vTabs = config.tabbrowser.visibleTabs;
-      let setList = liberator.plugins.exSelectd.exnum(args);
+      let setList = exnum(args);
       let arrayList = [...setList];
       let filteredList = arrayList.filter(e => (0 < e && e <= vTabs.length));
       let sortedList = filteredList.sort((a, b) => a < b);
 
       sortedList.forEach(e => config.tabbrowser.removeTab(vTabs[e - 1]));
-      liberator.echomsg("Closed Tab " + liberator.plugins.exSelectd.indexize(sortedList.reverse()).join(', '));
+      liberator.echomsg("Closed Tab " + indexize(sortedList.reverse()).join(', '));
     } else {
       config.tabbrowser.removeTab(gBrowser.mCurrentTab);
       liberator.echomsg("Closed Current Tab");
@@ -18,53 +18,51 @@ liberator.plugins.exSelectd = (function() {
     completer: function(context) {
       completion.buffer(context)[0]
     }
-  }
-  );
-  return {
-    exnum: function(numbers) {
-      let set = new Set();
-      for (let n of numbers) {
-        if (typeof (n) != 'number') {
-          let matches = n.match(/(\d+)-(\d+)/);
-          if (matches) {
-            let start = Math.min(matches[1], matches[2]);
-            let end = Math.max(matches[1], matches[2]);
-            let i = start;
-            while (i <= end) {
-              set.add(i++);
-            }
-            continue;
-          }
-          n = parseInt(n);
-        }
-        set.add(n);
-      }
-      return set;
-    },
+  });
 
-    indexize: function(ar) {
-      let out = new Array();
-      for (let i = 0; i < ar.length; i++) {
-        var a = i + 1;
-        var current = ar[i];
-        var concat = false;
-        while (parseInt(ar[a++]) == (current + 1)) {
-          current = ar[a - 1];
-          concat = true;
+  function indexize(numbers) {
+    let set = new Set();
+    for (let n of numbers) {
+      if (typeof (n) != 'number') {
+        let matches = n.match(/(\d+)-(\d+)/);
+        if (matches) {
+          let start = Math.min(matches[1], matches[2]);
+          let end = Math.max(matches[1], matches[2]);
+          let i = start;
+          while (i <= end) {
+            set.add(i++);
+          }
+          continue;
         }
-        if (concat) {
-          //if ( parseInt( ar[i] ) + 1 == current ) {
-          //	out.push( ar[i] );
-          //	out.push( ar[i + 1] );
-          //} else {
-          out.push(ar[i] + "-" + current);
-        //}
-        } else {
-          out.push(ar[i]);
-        }
-        i = a - 2;
+        n = parseInt(n);
       }
-      return out;
+      set.add(n);
     }
-  };
+    return set;
+  }
+
+  function exnum(ar) {
+    let out = new Array();
+    for (let i = 0; i < ar.length; i++) {
+      var a = i + 1;
+      var current = ar[i];
+      var concat = false;
+      while (parseInt(ar[a++]) == (current + 1)) {
+        current = ar[a - 1];
+        concat = true;
+      }
+      if (concat) {
+        //if ( parseInt( ar[i] ) + 1 == current ) {
+        //	out.push( ar[i] );
+        //	out.push( ar[i + 1] );
+        //} else {
+        out.push(ar[i] + "-" + current);
+      //}
+      } else {
+        out.push(ar[i]);
+      }
+      i = a - 2;
+    }
+    return out;
+  }
 })();
